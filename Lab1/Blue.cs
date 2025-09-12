ed7817a4c3d7f9e77624686cd62ebefd815804a1
Илья Киселёв <138257294+nakakorik@users.noreply.github.com>
2025-09-12 16:57:12 +0300

    Киселёв Илья БИВТ-25-15 Blue

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -7,7 +7,7 @@
             bool answer = false;
 
             // code here
-
+            if ( (a*b > 0) | (a==0 & b == 0) ) { answer = true; }
             // end
 
             return answer;
@@ -17,7 +17,10 @@
             bool answer = false;
 
             // code here
-
+            if (d - (int)d != 0)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -25,9 +28,10 @@
         public bool Task3(int a, int b)
         {
             bool answer = false;
-
             // code here
-
+            if ( b != 0){
+                if (a % b == 0) { answer = true; }
+            }
             // end
 
             return answer;
@@ -35,31 +39,60 @@
         public double Task4(double d, double f, double g)
         {
             double answer = 0;
-
+            
             // code here
+ 
+            double mx = Math.Max(Math.Abs(d), Math.Max( Math.Abs(f), Math.Abs(g)) );
+            if (mx == Math.Abs(d))
+            {
+                answer = d;
+            }
+            else if (mx == Math.Abs(f))
+            {
+                answer = f;
+            }
+            else if (mx == Math.Abs(g))
+            {
+                answer = g;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public double Task5(double x)
         {
             double answer = 0;
 
             // code here
+            if (x <= -1)
+            {
+                answer = 0;
+            }
+            else if (-1 < x & x <= 0)
+            {
+                answer = x + 1;
+            }
+            else if (x > 0)
+            {
+                answer = 1;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task6(double circleS, double squareS)
         {
             bool answer = false;
 
             // code here
-
+            double r = Math.Sqrt(circleS/Math.PI);
+            double l = Math.Sqrt(squareS);
+            if (2*r <= l)
+            {
+                answer = true;
+            }
             // end
-
+   
             return answer;
         }
 
@@ -68,17 +101,33 @@
             int answer = 0;
 
             // code here
+            if (Math.Abs(d) < Math.Abs(f))
+            {
+                if (d > 0) answer = -1;
+            }
+            else
+            {
+                if (f> 0) answer = 1;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task8(int a, int b, int c)
         {
             bool answer = false;
 
             // code here
-            
+            int val_a = (int)a / 2;
+            int val_b = (int)b / 2;
+            int val_c = (int)c / 2;
+            double total = val_a + val_b + val_c;
+            int mn = Math.Min(a, Math.Min(c,b));
+            double d = Double.Ceiling(total/3);
+            if (d*3 - total <= 1 && d>0 && d <= mn)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
